a5a1b9ecc4e2a477bfbcdb57b073e311b465c10d
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-04-12 12:11:18 +0530

    Invi logo link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,11 @@ const Navbar = ({ className }) => {
   return (
     <>
       <Header />
-      <a className="navbar-brand logo text-center w-100" href="#">
+      <a className="navbar-brand logo text-center w-100" href="/">
         <img src={img} alt="Invi Logo" width="150" />
       </a>
       <nav className="navbar navbar-expand-lg navbar-light container-fluid py-3">
-        <a className="navbar-brand d-lg-none" href="#">
+        <a className="navbar-brand d-lg-none" href="/">
           <img src={img} alt="Invi Logo" width="150" />
         </a>
         <button
