9af1ec3bc88307dc91fa4999459eeffd786ca4ff
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-04-20 19:39:32 +0530

    issue of example file

--- src/pages/example.js
+++ src/pages/example.js
@@ -1,8 +1,7 @@
 import React from "react"
-import "../components/main.css"
-import { graphql } from "gatsby"
-const example = ({ data }) => {
-  return <div>{data.contact.email}</div>
+
+const example = () => {
+  return <div></div>
 }
 
 export default example
