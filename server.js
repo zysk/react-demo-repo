18d347e223e35119f47bf081e23667a286670c02
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-04-30 10:13:13 +0530

    changed express

--- server.js
+++ server.js
@@ -25,52 +25,52 @@ const auth = {
 // Step 2
 let transporter = nodemailer.createTransport(mailGun(auth))
 
-exports.handler = async event => {
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: "Method Not Allowed",
-      headers: { Allow: "POST" },
-    }
-  }
+// exports.handler = async event => {
+//   if (event.httpMethod !== "POST") {
+//     return {
+//       statusCode: 405,
+//       body: "Method Not Allowed",
+//       headers: { Allow: "POST" },
+//     }
+//   }
 
-  const data = JSON.parse(event.body)
-  if (!data.comment || !data.name || !data.email) {
-    return { statusCode: 422, body: "Name, email, and comments are required." }
-  }
+//   const data = JSON.parse(event.body)
+//   if (!data.comment || !data.name || !data.email) {
+//     return { statusCode: 422, body: "Name, email, and comments are required." }
+//   }
 
-  const mailgunData = {
-    from: data.email,
-    to: CONTACT_TO_EMAIL_ADDRESS,
-    "h:Reply-To": data.email,
-    subject: `New contact from ${data.name}`,
-    text: `Name: ${data.name}\nEmail: ${data.email}\nComment: ${data.comment}`,
-  }
-  return mailgun
-    .comments()
-    .send(mailgunData)
-    .then(() => ({
-      statusCode: 200,
-      body: "Your form was sent successfully! We'll be in touch.",
-    }))
-    .catch(error => ({
-      statusCode: 422,
-      body: `Error: ${error}`,
-    }))
-}
+//   const mailgunData = {
+//     from: data.email,
+//     to: CONTACT_TO_EMAIL_ADDRESS,
+//     "h:Reply-To": data.email,
+//     subject: `New contact from ${data.name}`,
+//     text: `Name: ${data.name}\nEmail: ${data.email}\nComment: ${data.comment}`,
+//   }
+//   return mailgun
+//     .comments()
+//     .send(mailgunData)
+//     .then(() => ({
+//       statusCode: 200,
+//       body: "Your form was sent successfully! We'll be in touch.",
+//     }))
+//     .catch(error => ({
+//       statusCode: 422,
+//       body: `Error: ${error}`,
+//     }))
+// }
 
 // Step 3
-// let mailOptions = {
-//     from: 'abc@gmail.com', // TODO: email sender
-//     to: 'cba@gmail.com', // TODO: email receiver
-//     subject: 'Nodemailer - Test',
-//     text: 'Wooohooo it works!!'
-// };
+let mailOptions = {
+  from: "abc@gmail.com", // TODO: email sender
+  to: "cba@gmail.com", // TODO: email receiver
+  subject: "Nodemailer - Test",
+  text: "Wooohooo it works!!",
+}
 
 // Step 4
-// transporter.sendMail(mailOptions, (err, data) => {
-//     if (err) {
-//         return log('Error occurs');
-//     }
-//     return log('Email sent!!!');
-// });
+transporter.sendMail(mailOptions, (err, data) => {
+  if (err) {
+    return log("Error occurs")
+  }
+  return log("Email sent!!!")
+})
