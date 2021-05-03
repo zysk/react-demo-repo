e4924f57534696a6355a4757abae5079c15eb462
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-05-03 11:21:42 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,12 +57,27 @@ module.exports = {
     },
     `@contentful/gatsby-transformer-contentful-richtext`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-V4NJPDHJ12",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     // `gatsby-plugin-gatsby-cloud`,
