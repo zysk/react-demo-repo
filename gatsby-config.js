89a4ff9b44ed70035c79ff8c158b7f9e9148241f
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-05-12 12:15:54 +0530

    changed contentful preview url

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,7 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
       },
     },
     `@contentful/gatsby-transformer-contentful-richtext`,
