9b70dc4cbf048f6f2f7b88828e4238eb5ff69284
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-05-12 20:32:01 +0530

    browser files replaced

--- src/components/header.js
+++ src/components/header.js
@@ -22,6 +22,12 @@ const Header = () => {
         src="https://kit.fontawesome.com/ff32cb2647.js"
         crossOrigin="anonymous"
       ></script>
+
+      <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+      <script src="https://unpkg.com/aframe-layout-component@5.3.0/dist/aframe-layout-component.min.js"></script>
+      <script src="https://unpkg.com/aframe-troika-text/dist/aframe-troika-text.min.js"></script>
+      <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/aframe-glow@1.0.1/src/glow.min.js"></script>
       <script src="../../static/js/hero.js"></script>
       <script src="../../static/js/stars.js"></script>
       <script src="../../static/js/orb.js"></script>
