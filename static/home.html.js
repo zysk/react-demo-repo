0173a2b5a7dcaa10f710c416c12883acacc84eef
rishikalokesh2512 <rishikalokesh2512@gmail.com>
2021-05-25 11:56:11 +0530

    styles changed

--- static/home.html.js
+++ static/home.html.js
@@ -10,7 +10,7 @@ module.exports = `<!DOCTYPE html>
 	<script src="https://cdn.jsdelivr.net/npm/aframe-glow@1.0.1/src/glow.min.js"></script>
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="/css/aframe.css">
 
 	<script src="/js-1/hero.js"></script>
@@ -18,8 +18,7 @@ module.exports = `<!DOCTYPE html>
 	<script src="/js-1/orb.js"></script>
 	<script src="/js-1/lighting.js"></script>
 
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-		integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	
 
 </head>
 
